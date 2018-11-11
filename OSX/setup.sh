350c0eab11edd3c8cc8108282b1ee0726074466a
Marcelo Lazaroni <lazaronijunior@gmail.com>
2018-11-11 17:10:52 +0000

    Add more sensible speeds for mac keyboard

--- OSX/setup.sh
+++ OSX/setup.sh
@@ -1,10 +1,10 @@
 # This setup only needs to be ran once.
 
 # Set keys to start repeating after 100ms
-defaults write -g InitialKeyRepeat -int 5
+defaults write -g InitialKeyRepeat -int 10
 
 # Makes keys start repeating sooner
-defaults write -g KeyRepeat -int 1 
+defaults write -g KeyRepeat -int 1
 
 # Allow every key to be repeated
 defaults write -g ApplePressAndHoldEnabled -bool false
