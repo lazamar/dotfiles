1e247b88cf1bd6b24da39e6e2f20fd2d1a752ded
Marcelo Lazaroni <lazaronijunior@gmail.com>
2020-06-14 11:05:13 +0100

    Make my default expression lower case

--- .nix-defexpr/Marcelo/default.nix
+++ .nix-defexpr/Marcelo/default.nix
@@ -2,13 +2,14 @@ let
 	pkgs = import <nixpkgs> {};
 
 in pkgs.buildEnv {
-	name = "Marcelo";
+	name = "marcelo";
 
 	paths = with pkgs; [
 		git
 		fzf               # Fast file search
 		silver-searcher   # Fast string search
 		bat               # Syntax highlighting
+        powerline-rs      # Prompt with git info
 
 		vim
 
