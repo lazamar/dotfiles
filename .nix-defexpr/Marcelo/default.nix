4f0c3363c42e61e916202e7543224c32a2d0d8f1
Marcelo Lazaroni <lazaronijunior@gmail.com>
2020-06-14 10:51:17 +0100

    Cleanup default.nix

--- .nix-defexpr/Marcelo/default.nix
+++ .nix-defexpr/Marcelo/default.nix
@@ -1,32 +1,19 @@
-let 
+let
 	pkgs = import <nixpkgs> {};
 
 in pkgs.buildEnv {
 	name = "Marcelo";
 
 	paths = with pkgs; [
-		# Desktop environment
-		rxvt_unicode
-        	xmonad-with-packages
-
-		# Development environment
 		git
-		fzf
-		silver-searcher
-		bat # Syntax highlighting
+		fzf               # Fast file search
+		silver-searcher   # Fast string search
+		bat               # Syntax highlighting
 
-		# Editors
 		vim
-		vimPlugins.vim-plug
-        	sublime3
 
-		# Haskell
-		ghc
-		cabal-install
 		stack
 		haskellPackages.ghcid
 		haskellPackages.hlint
-		haskellPackages.hoogle
-		haskellPackages.stylish-haskell
 	];
-} 
+}
