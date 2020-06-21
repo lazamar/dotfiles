9529cd5959ee90564a3e74857af702fad00316f5
Marcelo Lazaroni <lazaronijunior@gmail.com>
2020-06-21 23:09:36 +0000

    Pin mosh nixpkgs

--- .nix-defexpr/marcelo.nix
+++ .nix-defexpr/marcelo.nix
@@ -9,12 +9,20 @@ let
          rev = "aaeb0a98536c1408c6f579d2afe7b9ff452d3987";
     }) {};
 
+    mosh-pkgs = import (builtins.fetchGit {
+         name = "revision-with-good-mosh";
+         url = "https://github.com/nixos/nixpkgs-channels/";
+         ref = "refs/heads/nixpkgs-unstable";
+         rev = "04d6123309fa5f89b436472d7b4fd418f8c3106f";
+     }) {};
+
     # Mosh has merged TrueColor support, but it hasn't had
     # a release after that. The last release was in 2017
-    mosh-truecolor = pkgs.mosh.overrideDerivation (old: {
+    # To support that we build bosh with a recent commit.
+    mosh-truecolor = mosh-pkgs.mosh.overrideDerivation (old: {
         name = "mosh-1.3.2-TrueColor";
 
-        src = pkgs.fetchurl {
+        src = mosh-pkgs.fetchurl {
             url = "https://github.com/mobile-shell/mosh/archive/03087e7a761df300c2d8cd6e072890f8e1059dfa.tar.gz";
             sha256 = "1v2qlh6jbngizg5c9sfrx4d79a9x3gmi9fjfxdzpjss2i8wkhl8z";
         };
