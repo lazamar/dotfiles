f84340a51e7faea3e275afd7ebafad5840d1f090
Marcelo Lazaroni <lazaronijunior@gmail.com>
2020-07-15 23:47:30 +0100

    Add annotations about interactive commands

--- linux-setup.sh
+++ linux-setup.sh
@@ -1,13 +1,15 @@
 # Instructions to setup a new Linux machine
 
 # Create a new user
-adduser marcelo
+adduser marcelo             # Needs to be made non-interactive
 
 # add it to sudoers
-sudo vim /etc/sudoers
+sudo vim /etc/sudoers       # Needs to be made non-interactive
 
 # Log into user account
-su marcelo
+su marcelo                  # Running the next commands as
+                            # the new user needs to be made
+                            # non-interactive
 
 # Install nix and cachix
 curl -L https://nixos.org/nix/install | sh
@@ -16,7 +18,7 @@ nix-env -i cachix
 cachix use cachix
 
 # create ssh keys
-ssh-keygen -t rsa -b 4096
+ssh-keygen -t rsa -b 4096    # Needs to be made non-interactive
 sudo cat /root/.ssh/authorized_keys > ~/.ssh/authorized_keys
 
 # Prepare home directory
@@ -28,6 +30,6 @@ cd ~ && git reset --hard
 nix-env -i marcelo
 
 # Vim extensions
-nvim -c PlugInstall
+nvim -c PlugInstall          # Needs to be made non-interactive
 
 
