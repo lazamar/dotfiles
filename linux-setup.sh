261cd72532f8dfd8a3c426ee0c54ae349bb26070
Marcelo Lazaroni <lazamar@users.noreply.github.com>
2020-08-24 23:25:59 +0100

    Add execution of commands as unprivileged user

--- linux-setup.sh
+++ linux-setup.sh
@@ -6,30 +6,27 @@ adduser marcelo             # Needs to be made non-interactive
 # add it to sudoers
 sudo vim /etc/sudoers       # Needs to be made non-interactive
 
-# Log into user account
-su marcelo                  # Running the next commands as
-                            # the new user needs to be made
-                            # non-interactive
+# Run commands as marcelo
+sudo -i -u marcelo bash << EOF
+        # Install nix and cachix
+        curl -L https://nixos.org/nix/install | sh
+        . /home/marcelo/.nix-profile/etc/profile.d/nix.sh
+        nix-env -i cachix
+        cachix use cachix
 
-# Install nix and cachix
-curl -L https://nixos.org/nix/install | sh
-. /home/marcelo/.nix-profile/etc/profile.d/nix.sh
-nix-env -i cachix
-cachix use cachix
+        # create ssh keys
+        ssh-keygen -t rsa -b 4096    # Needs to be made non-interactive
+        sudo cat /root/.ssh/authorized_keys > ~/.ssh/authorized_keys
 
-# create ssh keys
-ssh-keygen -t rsa -b 4096    # Needs to be made non-interactive
-sudo cat /root/.ssh/authorized_keys > ~/.ssh/authorized_keys
+        # Prepare home directory
+        cd ~ && git clone https://github.com/lazamar/dotfiles.git
+        cp -R ~/dotfiles/.git .
+        cd ~ && git reset --hard
 
-# Prepare home directory
-cd ~ && git clone https://github.com/lazamar/dotfiles.git
-cp -R ~/dotfiles/.git .
-cd ~ && git reset --hard
+        # Install programs
+        nix-env -i marcelo
 
-# Install programs
-nix-env -i marcelo
-
-# Vim extensions
-nvim -c PlugInstall          # Needs to be made non-interactive
-
+        # Vim extensions
+        nvim -c PlugInstall          # Needs to be made non-interactive q
+EOF
 
